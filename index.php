3475e85e405aed850b9b92c71b2bad4bf414cd48
henriquechd1234 <159437428+henriquechd1234@users.noreply.github.com>
2024-10-02 15:35:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>InfinityHUB</title>
-    <link rel="stylesheet" href="../css/ini.scss">
+    <link rel="stylesheet" href="../css/ini.css">
     <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon" a>
     
 </head>
