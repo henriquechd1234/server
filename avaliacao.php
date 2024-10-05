10b185a5014a2705973d8700994b2ccf56d1f5d9
henriquechd1234 <159437428+henriquechd1234@users.noreply.github.com>
2024-10-05 10:56:16 -0300

    Update avaliacao.php

--- avaliacao.php
+++ avaliacao.php
@@ -9,7 +9,7 @@ if(!isset($_SESSION)){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php $row['nome'] ?></title>
+    <title></title>
     <link rel="stylesheet" href="css/filme.css">
     <link rel="stylesheet" href="css/ini.css">
 </head>
