bea69485a133a4a884faa617f6bb3b782bd3d95c
David Martins <83629031+daviddsbb@users.noreply.github.com>
2024-10-07 23:51:36 -0300

    Update ini.js

--- js/ini.js
+++ js/ini.js
@@ -24,3 +24,25 @@ topTenGrid.addEventListener('wheel', (event) => {
         behavior: 'smooth'
     });
 });
+
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelector('.slider');
+    const totalSlides = slides.children.length;
+
+    currentSlide += direction;
+
+    if (currentSlide < 0) {
+        currentSlide = totalSlides - 1;
+    } else if (currentSlide >= totalSlides) {
+        currentSlide = 0;
+    }
+
+    const offset = -currentSlide * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
+setInterval(() => {
+    moveSlide(1);
+}, 5000);
