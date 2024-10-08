a4371b77c89370874aa210504d8085939cba46ce
henriquechd1234 <159437428+henriquechd1234@users.noreply.github.com>
2024-10-08 19:15:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -78,6 +78,8 @@ echo '</div>';
                 }
             }
         }else{
+            $code_filmes = "SELECT * FROM imagens LIMIT 10";
+            $sql_query = $mysqli -> query($code_filmes);
         ?>
      <hr class="espacamento">
         <div class="top-ten-section">
@@ -85,88 +87,17 @@ echo '</div>';
             <div class="top-ten-container">
                 <button class="prev-button">&#10094;</button> 
                 <div class="top-ten-grid">
+                    <?php while ($row2 = $sql_query -> fetch_assoc()){ ?>
                     
                     <div class="movie-card">
-                        <img src="../capaFILMES/1.jpg" alt="Alien: Romulus">
+                        <img src="<?php echo $row2['id']; ?>" alt="">
                         <div class="movie-info">
-                            <h3>1. Alien: Romulus</h3>
-                            <button>+ Lista</button>
-                            <button>Trailer</button>
+                            <h3><?php echo $row2 ['nome']?></h3>
                             <a href="avaliacao.php"><button>Avaliar</button></a>
                         </div>
                     </div>
-                    <div class="movie-card">
-                        <img src="../capaFILMES/2.jpeg" alt="Deadpool & Wolverine">
-                        <div class="movie-info">
-                            <h3>2. Deadpool & Wolverine</h3>
-                            <button>+ Lista</button>
-                            <button>Trailer</button>
-                        </div>
-                    </div>
-                    <div class="movie-card">
-                        <img src="../capaFILMES/3.jpg" alt="A Liga">
-                        <div class="movie-info">
-                            <h3>3. A Liga</h3>
-                            <button>+ Lista</button>
-                            <button>Trailer</button>
-                        </div>
-                    </div>
-                    <div class="movie-card">
-                        <img src="../capaFILMES/4.jpg" alt="É Assim que Acaba">
-                        <div class="movie-info">
-                            <h3>4. É Assim que Acaba</h3>
-                            <button>+ Lista</button>
-                            <button>Trailer</button>
-                        </div>
-                    </div>
-                    <div class="movie-card">
-                        <img src="../capaFILMES/5.png" alt="Furiosa: Uma Saga...">
-                        <div class="movie-info">
-                            <h3>5. Furiosa: Uma Saga...</h3>
-                            <button>+ Lista</button>
-                            <button>Trailer</button>
-                        </div>
-                    </div>
-                    <div class="movie-card">
-                        <img src="../capaFILMES/6.jpeg" alt="Twisters">
-                        <div class="movie-info">
-                            <h3>6. Twisters</h3>
-                            <button>+ Lista</button>
-                            <button>Trailer</button>
-                        </div>
-                    </div>
-                    <div class="movie-card">
-                        <img src="../capaFILMES/7.jpg" alt="Duna: Parte 2">
-                        <div class="movie-info">
-                            <h3>7. Duna: Parte 2</h3>
-                            <button>+ Lista</button>
-                            <button>Trailer</button>
-                        </div>
-                    </div>
-                    <div class="movie-card">
-                        <img src="../capaFILMES/8.jpg" alt="Stree 2: Sarkate Ka">
-                        <div class="movie-info">
-                            <h3>8. Stree 2: Sarkate Ka</h3>
-                            <button>+ Lista</button>
-                            <button>Trailer</button>
-                        </div>
-                    </div>
-                    <div class="movie-card">
-                        <img src="../capaFILMES/9.jpg" alt="Kalki 2898-AD">
-                        <div class="movie-info">
-                            <h3>9. Kalki 2898-AD</h3>
-                            <button>+ Lista</button>
-                            <button>Trailer</button>
-                        </div>
-                    </div>
-                    <div class="movie-card">
-                        <img src="../capaFILMES/10.jpeg" alt="A Casa do Dragão">
-                        <div class="movie-info">
-                            <h3>10. A Casa do Dragão</h3>
-                            <button>+ Lista</button>
-                            <button>Trailer</button>
-                        </div>
-                    </div>
+                    <?php } ?>
+                    
                      <p class="espacamento"></p>
                     </div>
                 <button class="next-button">&#10095;</button> 
