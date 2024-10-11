1ed6bedce4f48d5ac1a527652906f522ded992e0
henriquechd1234 <159437428+henriquechd1234@users.noreply.github.com>
2024-10-11 15:48:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -119,11 +119,11 @@
             <div class="slide">
                 <?php foreach ($baners as $baner): ?>
                 <img src="<?php echo $baner['url']; ?>" alt="The Batman">
+    <?php endforeach; ?>
                 <div class="slide-info">
                     <h3>Filme da Semana #1</h3>
                     <h2>AranhaVerso</h2>
                 </div>
-    <?php endforeach; ?>
                 <br>
             </div>
         </div>
