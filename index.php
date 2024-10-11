c98755a8f536c375966072f9f3c184a0460aaa2e
henriquechd1234 <159437428+henriquechd1234@users.noreply.github.com>
2024-10-11 16:28:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -123,8 +123,8 @@
                     <h3>Filme da Semana #1</h3>
                     <h2><?php echo $baner['nome'] ?></h2>
                 </div>
-            </div>
            <?php endforeach; ?>
+            </div>
             <br>
         </div>
     </div>
