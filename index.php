324810b4e7acdb26dda6bd50b8538168cf83f412
David Martins <83629031+daviddsbb@users.noreply.github.com>
2024-10-12 16:22:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>InfinityHUB</title>
     <link rel="stylesheet" href="css/ini.css">
+    <link rel="stylesheet" href="css/reset.css">
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" a>
 
 </head>
